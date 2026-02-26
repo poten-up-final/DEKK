51f277d625a380d16d31754081c8360063401806
woongblack <woongblack123@gmail.com>
2026-02-27 00:25:20 +0900

    feat(security): JWT 예외 처리 구현

---
+++ src/main/java/com/dekk/security/jwt/exception/JwtBusinessException.java
@@ -0,0 +1,10 @@
+package com.dekk.security.jwt.exception;
+
+import com.dekk.common.error.BusinessException;
+import com.dekk.common.error.ErrorCode;
+
+public class JwtBusinessException extends BusinessException {
+    public JwtBusinessException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
