c919567d8394a893479bb04122a2bd6371f086bd
woongblack <woongblack123@gmail.com>
2026-03-02 04:21:17 +0900

    fix: CI 빌드 에러 해결을 위한 H2 격리 환경 세팅

--- src/test/java/com/dekk/DekkApplicationTests.java
+++ src/test/java/com/dekk/DekkApplicationTests.java
@@ -2,8 +2,10 @@ package com.dekk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DekkApplicationTests {
 
 	@Test
