60d1c8fab830f7c654198dba625820fe08d304ce
woongblack <woongblack123@gmail.com>
2026-03-07 15:16:47 +0900

    feat(auth):토큰 재발급 및 블랙리스트 기반 로그아웃 API 엔드포인트 구현

--- src/main/java/com/dekk/auth/application/AuthCommandService.java
+++ src/main/java/com/dekk/auth/application/AuthCommandService.java
@@ -2,6 +2,11 @@ package com.dekk.auth.application;
 
 import com.dekk.auth.application.command.TokenRefreshCommand;
 import com.dekk.auth.application.dto.result.TokenRefreshResult;
+import com.dekk.auth.domain.exception.AuthBusinessException;
+import com.dekk.auth.domain.exception.AuthErrorCode;
+import com.dekk.auth.domain.model.BlacklistAccessToken;
+import com.dekk.auth.domain.repository.BlacklistRepository;
+import com.dekk.auth.domain.repository.RefreshTokenRepository;
 import com.dekk.auth.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -12,9 +17,15 @@ import org.springframework.stereotype.Service;
 public class AuthCommandService {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final BlacklistRepository blacklistRepository;
 
     public TokenRefreshResult refreshToken(TokenRefreshCommand command) {
         jwtTokenProvider.validateToken(command.refreshToken());
+
+        if(!jwtTokenProvider.isRefreshToken(command.refreshToken())) {
+            throw new AuthBusinessException(AuthErrorCode.INVALID_REFRESH_TOKEN);
+        }
 
         Authentication authentication = jwtTokenProvider.getAuthentication((command.refreshToken()));
 
@@ -22,5 +33,16 @@ public class AuthCommandService {
         String newRefreshToken = jwtTokenProvider.createRefreshToken(authentication);
 
         return new TokenRefreshResult(newAccessToken, newRefreshToken);
+    }
+
+    public void logout(Long UserId, String accessToken) {
+        refreshTokenRepository.deleteByUserId(UserId);
+
+        long remainingExpirationTime = jwtTokenProvider.getRemainingExpirationTime(accessToken);
+
+        if (remainingExpirationTime > 0) {
+            BlacklistAccessToken blacklistAccessToken = BlacklistAccessToken.create(accessToken);
+            blacklistRepository.save(blacklistAccessToken, remainingExpirationTime);
+        }
     }
 }
