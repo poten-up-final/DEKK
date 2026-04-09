82258f5c285ef0c21e39f72d9c1d00b2e501d953
youable <youable.framios@gmail.com>
2026-04-09 16:09:35 +0900

    feat: 코드 리뷰 반영
    - 사용되지 않는 환경 변수 제거
    - 사용되지 않는 에러 코드 제거

--- src/main/java/com/dekk/app/admin/domain/exception/AdminErrorCode.java
+++ src/main/java/com/dekk/app/admin/domain/exception/AdminErrorCode.java
@@ -9,8 +9,6 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum AdminErrorCode implements ErrorCode {
     INVALID_ADMIN_DATA(HttpStatus.BAD_REQUEST, "EAD40001", "관리자 필수 정보가 누락되었거나 올바르지 않습니다."),
-    INVALID_TARGET_INSPECTION_STATUS(
-            HttpStatus.BAD_REQUEST, "EAD40003", "관리자는 승인(ADMIN_APPROVED) 또는 반려(ADMIN_REJECTED) 상태로만 변경할 수 있습니다."),
 
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "EAD40101", "비밀번호가 일치하지 않습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "EAD40102", "유효하지 않은 관리자 토큰입니다."),
