e6b2561be3d450ac67bbb2583627d04a0eba457e
youable <youable.framios@gmail.com>
2026-04-12 22:02:22 +0900

    fix: S3Client 내부적으로 관리하는 DefaultCredentialsProvider 사용하도록 변경

--- src/main/java/com/dekk/app/resource/infrastructure/storage/S3Config.java
+++ src/main/java/com/dekk/app/resource/infrastructure/storage/S3Config.java
@@ -3,7 +3,6 @@ package com.dekk.app.resource.infrastructure.storage;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -16,7 +15,6 @@ public class S3Config {
     public S3Client s3Client(StorageProperties properties) {
         return S3Client.builder()
                 .region(Region.of(properties.getRegion()))
-                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 
@@ -24,7 +22,6 @@ public class S3Config {
     public S3Presigner s3Presigner(StorageProperties properties) {
         return S3Presigner.builder()
                 .region(Region.of(properties.getRegion()))
-                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
