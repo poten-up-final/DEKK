bf8bca5b0e34c88f5bd1c6c4ffeeaefe8ec89b6c
Sung Ryul Cho <hahahoho5811@gmail.com>
2026-04-16 22:21:20 +0900

    chore(config): 이메일 발송 라이브러리 추가 및 비동기(Async) 환경 설정

--- src/main/java/com/dekk/global/security/jwt/TokenBlacklistManager.java
+++
@@ -1,6 +0,0 @@
-package com.dekk.global.security.jwt;
-
-public interface TokenBlacklistManager {
-
-    boolean isBlacklisted(String token);
-}
---
+++ src/main/resources/db/migration/V9__add_department_to_admins.sql
@@ -0,0 +1,5 @@
+ALTER TABLE admins
+    ADD COLUMN department VARCHAR(255);
+
+ALTER TABLE admins
+    ADD COLUMN status VARCHAR(50);
