d9849433b954d9b32a1d236280c5e6ed0e3d2068
Sung Ryul Cho <hahahoho5811@gmail.com>
2026-04-16 23:05:07 +0900

    style: 코드 포맷팅 자동 정렬 (spotlessApply)

--- src/main/java/com/dekk/global/security/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/dekk/global/security/jwt/filter/JwtAuthenticationFilter.java
@@ -17,10 +17,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.util.Arrays;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
@@ -56,9 +54,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final AdminTokenBlackListRepository adminTokenBlackListRepository;
 
     public JwtAuthenticationFilter(
-        JwtTokenProvider jwtTokenProvider,
-        ObjectMapper objectMapper,
-        AdminTokenBlackListRepository adminTokenBlackListRepository) {
+            JwtTokenProvider jwtTokenProvider,
+            ObjectMapper objectMapper,
+            AdminTokenBlackListRepository adminTokenBlackListRepository) {
         this.jwtTokenProvider = jwtTokenProvider;
         this.objectMapper = objectMapper;
         this.adminTokenBlackListRepository = adminTokenBlackListRepository;
@@ -77,7 +75,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
 
         String requestUri = request.getRequestURI();
         String jwt = null;
@@ -126,10 +124,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             return Arrays.stream(cookies)
-                .filter(cookie -> targetCookieName.equals(cookie.getName()))
-                .map(Cookie::getValue)
-                .findFirst()
-                .orElse(null);
+                    .filter(cookie -> targetCookieName.equals(cookie.getName()))
+                    .map(Cookie::getValue)
+                    .findFirst()
+                    .orElse(null);
         }
         return null;
     }
